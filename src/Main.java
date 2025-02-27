842d4ec46bdd70cee5b34bd27b5519c475afb328
King-Rue Liew <rswift@KRyls-MacBook-Pro.local>
2025-02-26 19:32:03 -0800

    update exchange in main and add more tests in main

--- src/Main.java
+++ src/Main.java
@@ -11,27 +11,25 @@ public class Main {
         // Test 'in' method of Cash
         Cash euro = dollar.in("Euro");
         System.out.println("Dollar to Euro: " + euro.toString());
-        Cash usd = dollar.in("USD");
-        System.out.println("Dollar to Dollar: " + usd.toString());
-        Cash jpy = dollar.in("JPY");
-        System.out.println("Dollar to JPY: " + jpy.toString());
+        // Cash usd = dollar.in("USD");
+        // System.out.println("Dollar to Dollar: " + usd.toString());
+        // Cash jpy = dollar.in("JPY");
+        // System.out.println("Dollar to JPY: " + jpy.toString());
     }
 }
 
 //----------------Fake Exchange for Cash, Replace NYSE with FakeExchange---------------------------------
 /**
  * Class FakeExchange is to avoid external API calls
+ * return a fixed exchange rate for unit testing
  */
 class FakeExchange implements Exchange {
     @Override
     public float rate(String origin, String target) {
         //System.out.println("Exchange requested: " + origin + " to " + target); // debug
-        // return a fixed exchange rate for unit testing
-        if (origin.equals("USD") && target.equals("Euro")) {
-            return 0.2f; // assume 1 USD = 0.2 Euro
-        } else if (origin.equals(target)){
-            return 1.0f; // if same currency, so 1:1 conversion
-        }
-        throw new IllegalArgumentException("Exchange rate is not available for " + origin + " to " + target);
+        if (origin.equals(target)) {
+            return 1.0f; 
+        } 
+        return 0.2f; // assume 1 USD = 0.2 Euro or other currency
     }
 }
